eeecbef858be25f90743553508cd95de41c80175
Simmeon <you@example.com>
2017-05-19 04:32:14 +0300

    Added delete-photo dunctionality and some photo visualization fixes

--- app/src/main/java/bg/o/sim/taggerizmus/DetailsActivity.java
+++ app/src/main/java/bg/o/sim/taggerizmus/DetailsActivity.java
@@ -1,16 +1,20 @@
 package bg.o.sim.taggerizmus;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.content.FileProvider;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -27,7 +31,7 @@ import java.util.Arrays;
 /**
  * Displays a location-marker's details and provides an interface for modifying said details.
  */
-public class DetailsActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity implements PhotoDialog.PhotoDialogActionListener {
 
     private static final byte REQUEST_CODE_TAKE_PHOTO = 1;
     //Camera activity should return the photo in extras, under this key.
@@ -58,65 +62,7 @@ public class DetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_details);
         dbAdapter = DbAdapter.getInstance(this);
 
-
-        long id = getIntent().getLongExtra(getString(R.string.EXTRA_MARKER), -1);
-        if (id == -1) finish(); //TODO - ! decide on how to notify user... gently... that something went wrong. !
-        marker = dbAdapter.getMarker(id);
-        if (marker == null) finish(); //TODO - ! decide on how to notify user... gently... that something ELSE went wrong. !
-        details = (MarkerDetail) marker.getTag(); //TODO - this needs a check too :/
-
-        IMAGE_FOLDER = getString(R.string.FILE_MARKER_IMAGE_FOLDER) + details.getId();
-
-
-
-        address = (TextView) findViewById(R.id.details_address);
-        country = (TextView) findViewById(R.id.details_country);
-
-        latitude = (TextView) findViewById(R.id.details_lon);
-        longitude = (TextView) findViewById(R.id.details_lat);
-
-        defaultPhoto = (ImageView) findViewById(R.id.details_default_photo);
-        photoPager = (ViewPager) findViewById(R.id.details_photo_pager);
-        images = new ArrayList<>();
-
-        final File imageDir = new File(getFilesDir() + IMAGE_FOLDER);
-
-        if (imageDir.isFile()) imageDir.delete();
-        if (!imageDir.exists()) imageDir.mkdirs();
-
-        if (imageDir.listFiles().length > 0) {
-            images.addAll(Arrays.asList(imageDir.listFiles()));
-            defaultPhoto.setVisibility(View.GONE);
-            photoPager.setVisibility(View.VISIBLE);
-        }
-        else {
-            photoPager.setVisibility(View.GONE);
-            defaultPhoto.setVisibility(View.VISIBLE);
-        }
-
-        photoPager.setAdapter(new PagerAdapter() {
-            @Override
-            public int getCount() {
-                return images.size();
-            }
-
-            @Override
-            public boolean isViewFromObject(View view, Object object) {
-                return view.equals((View)object);
-            }
-
-            @Override
-            public Object instantiateItem(ViewGroup container, int position) {
-                ImageView imageView = new ImageView(DetailsActivity.this);
-                imageView.setImageURI(Uri.fromFile(images.get(position)));
-                container.addView(imageView);
-                return imageView;
-            }
-        });
-
-
-        photoButton = (Button) findViewById(R.id.details_add_photo);
-        photoButton.setOnClickListener(new View.OnClickListener() {
+        View.OnClickListener photoListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent camIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -138,7 +84,87 @@ public class DetailsActivity extends AppCompatActivity {
                     }
                 }
             }
+        };
+
+        
+        long id = getIntent().getLongExtra(getString(R.string.EXTRA_MARKER), -1);
+        if (id == -1) finish(); //TODO - ! decide on how to notify user... gently... that something went wrong. !
+        marker = dbAdapter.getMarker(id);
+        if (marker == null) finish(); //TODO - ! decide on how to notify user... gently... that something ELSE went wrong. !
+        details = (MarkerDetail) marker.getTag(); //TODO - this needs a check too :/
+
+        IMAGE_FOLDER = getString(R.string.FILE_MARKER_IMAGE_FOLDER) + details.getId();
+
+        address = (TextView) findViewById(R.id.details_address);
+        country = (TextView) findViewById(R.id.details_country);
+
+        latitude = (TextView) findViewById(R.id.details_lon);
+        longitude = (TextView) findViewById(R.id.details_lat);
+
+        defaultPhoto = (ImageView) findViewById(R.id.details_default_photo);
+        defaultPhoto.setOnClickListener(photoListener);
+
+        photoPager = (ViewPager) findViewById(R.id.details_photo_pager);
+        images = new ArrayList<>();
+
+        final File imageDir = new File(getFilesDir() + IMAGE_FOLDER);
+
+        if (imageDir.isFile()) imageDir.delete();
+        if (!imageDir.exists()) imageDir.mkdirs();
+
+        if (imageDir.listFiles().length > 0) {
+            images.addAll(Arrays.asList(imageDir.listFiles()));
+            defaultPhoto.setVisibility(View.GONE);
+            photoPager.setVisibility(View.VISIBLE);
+        } else {
+            photoPager.setVisibility(View.GONE);
+            defaultPhoto.setVisibility(View.VISIBLE);
+        }
+
+        photoPager.setAdapter(new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return images.size();
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view.equals((View)object);
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, final int position) {
+                ImageView imageView = new ImageView(DetailsActivity.this);
+                imageView.setImageURI(Uri.fromFile(images.get(position)));
+
+                imageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        PhotoDialog dialog = new PhotoDialog(DetailsActivity.this ,images.get(position), DetailsActivity.this ); //TODO - could combine the Context and Listener in 1 param.
+                        dialog.show();
+                        Window window = dialog.getWindow();
+                        window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+                    }
+                });
+
+                container.addView(imageView);
+                return imageView;
+            }
+
+            @Override
+            public int getItemPosition(Object object) {
+                return images.contains(object) ? images.indexOf(object) : POSITION_NONE;
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                photoPager.removeView((View)object);
+            }
         });
+
+
+        photoButton = (Button) findViewById(R.id.details_add_photo);
+        photoButton.setOnClickListener(photoListener);
 
         address.setText(details.getAddress());
         country.setText(details.getCountry());
@@ -164,10 +190,22 @@ public class DetailsActivity extends AppCompatActivity {
 
     private File createImageFile() throws IOException {
 
-        // Evaluates to sth like images/marker_1/1495144334391
         String fileName = (System.currentTimeMillis() + "");
+
+        // Evaluates to sth like /images/marker_8/1495144334391
         File image = new File(getFilesDir() + IMAGE_FOLDER, fileName);
 
         return image;
+    }
+
+    @Override
+    public void reactToDeletion(File imageFile) {
+        images.remove(imageFile);
+        photoPager.getAdapter().notifyDataSetChanged();
+        imageFile.delete();
+        if (images.size() == 0){
+            defaultPhoto.setVisibility(View.VISIBLE);
+            photoPager.setVisibility(View.GONE);
+        }
     }
 }
---
+++ app/src/main/java/bg/o/sim/taggerizmus/PhotoDialog.java
@@ -0,0 +1,88 @@
+package bg.o.sim.taggerizmus;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.Date;
+
+public class PhotoDialog extends Dialog {
+
+    private File imageFile;
+    private ImageButton deleteButton, returnButton;
+    private ImageView photo;
+    private TextView date;
+    private Context c;
+    private PhotoDialogActionListener listener;
+
+
+    public PhotoDialog(@NonNull Context context, File imageFile, PhotoDialogActionListener listener) {
+        super(context);
+
+        if (imageFile == null || !imageFile.exists() || imageFile.isDirectory())
+            throw new IllegalArgumentException("The File parameter MUST be an image file!");
+        if (listener == null)
+            throw new IllegalArgumentException("The listener parameter MUST be non-null!");
+
+        this.c = context;
+        this.listener = listener;
+        this.imageFile = imageFile;
+    }
+
+@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.dialog_photo);
+
+        deleteButton = (ImageButton) findViewById(R.id.dialog_photo_delete);
+        returnButton = (ImageButton) findViewById(R.id.dialog_photo_return);
+
+        photo = (ImageView) findViewById(R.id.dialog_photo_image);
+        photo.setImageURI(Uri.fromFile(imageFile));
+
+        date = (TextView) findViewById(R.id.dialog_photo_date);
+        date.setText((new Date(Long.parseLong(imageFile.getName())).toString()));   //TODO add DateFormat, to make it a bit more sightly
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(c)
+                        .setIcon(R.mipmap.ic_delete)
+                        .setMessage(c.getString(R.string.message_confirm_delete))
+                        .setPositiveButton("Yes", new OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                listener.reactToDeletion(imageFile);
+                                PhotoDialog.this.dismiss();
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+            }
+        });
+
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+    }
+
+
+        public interface PhotoDialogActionListener {
+        void reactToDeletion(File imageFile);
+    };
+}
+
